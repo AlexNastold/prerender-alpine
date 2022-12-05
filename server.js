336528d383ca3a0f1f2c96a5e293b23ed14184bd
Alex Nastold <alexander.nastold@gmail.com>
2022-12-05 13:33:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const prerender = require('prerender');
 const prMemoryCache = require('prerender-memory-cache');
+const prDiskCache = require('prerender-disk-cache');
 
 const server = prerender({
     chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
@@ -12,6 +13,11 @@ const server = prerender({
 const memCache = Number(process.env.MEMORY_CACHE) || 0;
 if (memCache === 1) {
     server.use(prMemoryCache);
+}
+
+const diskCache = Number(process.env.DISK_CACHE) || 0;
+if (diskCache === 1) {
+    server.use(prDiskCache);
 }
 
 server.use(prerender.blacklist());
