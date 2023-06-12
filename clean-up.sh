374f5d50c8562f2f43ec573389ddfbd94c4d2f62
a.nastold <a.nastold@fis-gmbh.de>
2023-06-12 15:38:38 +0200

    Remove clean-up

--- clean-up.sh
+++
@@ -1,9 +0,0 @@
-FILES="/prerender/cache/**/*.html"
-for f in $FILES
-do
-  if cat "$f" | grep -q "<title>Harolds - FIS/eSales dev</title>"; then
-    rm "$f"
-    echo "Removed: $f"
-  fi
-done
-find /prerender/cache  -empty -type d -delete
